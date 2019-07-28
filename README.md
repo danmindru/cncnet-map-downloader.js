493441de4da449261ca0a386c938b6554f6aa4b8
Dan Mindru <mindrudan@gmail.com>
2019-07-28 16:04:02 +0200

    Skip downloading existing maps

--- README.md
+++ README.md
@@ -1,15 +1,29 @@
 # cncnet-map-downloader.js
 Downloads all maps from [cncnet.org](https://cncnet.org) into a directory of your choice.
 
-By default, it only downloads Yuri (yr) maps, but can be configured to download other types.
+### Usage
+Grab the latest binary release on [the release page](https://github.com/DHI/html-import-updater/releases) 💽.
 
-Download the latest binary release for your OS on [the release page](https://github.com/DHI/html-import-updater/releases).
+Double-click your favorite executable (win, mac, linux).
 
+- By default, it only downloads Yuri (yr) maps, but can be configured to download other types.
+- If the process is somehow killed, it will resume downloading maps that haven't been downloaded yet.
 
+#### Build it yourself
+Clone the repo, then run `npm install`. <br/>
+You can start a download with `npm run download`. Downloads can be configured in `package.json` under scripts.
 
 ### Configuration
+- `RUN_UNPACKAGED` - run the script in unpacked mode[1] (default: `false`; see `npm run download` for an example)
+- `GAME_TYPE` - the type of maps to download (default: `yr`)
+- `DESTINATION_DIR` - the location to download files (default: `cncnet-maps`)
+
+> [1] essentially, run the non-binary script. Requires node v10+ & `npm install`
 
 ### About the method
 Uses the cncnet search endpoint to retrieve all existing maps, then requests each map zip individually with a delay to be nice on the server.
 
 Previously, scarping was considered, but not necessary at this point in time.
+
+### Other art
+Check out the [cncnet search](https://mapdb.cncnet.org/search/?game=yr&search=), where you can search and download maps.
--- scripts/get-maps.js
+++ scripts/get-maps.js
@@ -8,7 +8,7 @@ const sanitize = require('sanitize-filename');
 
 const shouldScrape = process.env.FORCE_SCAPE;
 const gameType = process.env.GAME_TYPE || 'yr';
-const destinationDir = process.env.DESTINATION_DIR || 'cncnet-yuri-maps';
+const destinationDir = process.env.DESTINATION_DIR || 'cncnet-maps';
 const delayBetweenRequests = process.env.REQUEST_DELAY || 500;
 const debug = process.env.DEBUG;
 const cwd = process.env.RUN_UNPACKAGED ? process.cwd() : path.dirname(process.execPath);
@@ -22,9 +22,19 @@ if (!fs.existsSync(destinationDirAbsolutePath)) {
 }
 
 /**
+ * Builds a file name, escaping strange chars & appending the hash.
+ *
+ * @param { string } name
+ * @param { string } hash
+ * @param { string } filePath
+ */
+const buildFileName = (name, hash, filePath) =>
+  sanitize(name + ` {{${hash}}}` + filePath.slice(filePath.lastIndexOf('.') || '.map'));
+
+/**
  * Wraps a fs.writeFile call into a promise.
  *
- * @param { ScrollSetting } name
+ * @param { string } name
  * @param { Buffer } buffer
  */
 const writeFileAsync = (fileName, buffer) =>
@@ -58,7 +68,7 @@ const unzipAsync = ({ name, hash } = {}) => () =>
       async (directory) =>
         await directory.files.map(async (file) => {
           const buffer = await file.buffer();
-          const prettyFileName = `${destinationDir}/${sanitize(name + file.path.slice(file.path.lastIndexOf('.')))}`;
+          const prettyFileName = `${destinationDirAbsolutePath}/${buildFileName(name, hash, file.path)}`;
           await writeFileAsync(prettyFileName, buffer);
           return hash;
         })
@@ -69,6 +79,23 @@ const unzipAsync = ({ name, hash } = {}) => () =>
       }
       throw hash;
     });
+
+/**
+ * Filters an array of maps by maps already present in the 'file list'.
+ *
+ * @param { Array<string> } destinationDirFilelist
+ */
+const filterByExistingMaps = (destinationDirFilelist) => ({ hash }) =>
+  !destinationDirFilelist.some((fileName) => {
+    const matches = fileName.match(new RegExp(`{{(${hash})}}`, 'gi'));
+
+    if (matches) {
+      // If file matches and, presumably, has all the content written, skip it.
+      const stats = fs.statSync(`${destinationDirAbsolutePath}/${fileName}`);
+      const fileSizeInBytes = stats.size;
+      return fileSizeInBytes > 0;
+    }
+  });
 
 /**
  * Lists all maps then downloads them & unzips into the dir of choice.
@@ -83,12 +110,18 @@ const main = async () => {
   }
 
   await axios.get(`https://mapdb.cncnet.org/search-json.php?game=${gameType}&age=12`).then((res) => {
-    const mapRequests = res.data.map(unzipAsync);
+    const destinationDirFilelist = fs.readdirSync(destinationDirAbsolutePath);
+    const mapRequests = res.data.filter(filterByExistingMaps(destinationDirFilelist)).map(unzipAsync);
+    const filesSkipped = res.data.length - mapRequests.length - 1;
+
+    if (filesSkipped > 0) {
+      console.log(`\nSkipped ${destinationDirFilelist.length} existing maps.`);
+    }
 
     const handleUnzipPromise = (unzipPromise) => () => {
       process.stdout.clearLine();
       process.stdout.cursorTo(0);
-      process.stdout.write(`Downloading ${filesWrote.length + filesErrored.length + 1}/${res.data.length}`);
+      process.stdout.write(`Downloading ${filesWrote.length + filesErrored.length + 1}/${mapRequests.length}`);
       unzipPromise()
         .then((hashes) => filesWrote.push(...hashes))
         .catch((hash) => filesErrored.push(hash));
