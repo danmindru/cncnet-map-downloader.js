6b8379cc66317a3a5ad34fb5c5e1a4f725938b82
Dan Mindru <mindrudan@gmail.com>
2019-08-04 19:39:47 +0200

    Replace . with - in release file names

--- package.js
+++ package.js
@@ -4,19 +4,20 @@ const fs = require('fs');
 const packageDir = './packages';
 const entry = './scripts/get-maps.js';
 const targets = ['node10-linux-x64', 'node10-macos-x64', 'node10-win-x64'];
+const baseName = `${name}-${version}`.replace(/\./g, '-');
 
 if (!fs.existsSync(packageDir)) {
   fs.mkdirSync(packageDir);
 }
 
 const packageCode = async () => {
-  await exec([entry, '--target', targets[0], '--output', `${packageDir}/${name}-${version}-linux`]);
+  await exec([entry, '--target', targets[0], '--output', `${packageDir}/${baseName}-linux`]);
   console.log(`Packaged for linux`);
 
-  await exec([entry, '--target', targets[1], '--output', `${packageDir}/${name}-${version}-macos`]);
+  await exec([entry, '--target', targets[1], '--output', `${packageDir}/${baseName}-macos`]);
   console.log(`Packaged for mac`);
 
-  await exec([entry, '--target', targets[2], '--output', `${packageDir}/${name}-${version}-windows.exe`]);
+  await exec([entry, '--target', targets[2], '--output', `${packageDir}/${baseName}-windows.exe`]);
   console.log(`Packaged for windows`);
 };
 
